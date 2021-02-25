914ad5631673d4bd62292c707ea6668af7723f88
Koki Ohira <kouking527@icloud.com>
2021-02-25 18:17:05 +0900

    Add bootstrap and arrange structure of view
    To ensure maintainability

--- spec/requests/static_pages_requests_spec.rb
+++ spec/requests/static_pages_requests_spec.rb
@@ -17,25 +17,11 @@ RSpec.describe "StaticPages", type: :request do
     end
   end
 
-  describe "GET /static_pages/home" do
-    before do
-      get static_pages_home_url
-    end
-
-    it "render home" do
-      expect(response.status).to eq 200
-    end
-
-    it 'have page_title of title tag' do
-      expect(response.body).to match(/<title>#{base_title}<\/title>/i)
-    end
-  end
-
-  describe "GET /static_pages/help" do
+  describe "GET /help" do
     let(:page_title) { 'Help' }
 
     before do
-      get static_pages_help_url
+      get help_url
     end
 
     it "render help" do
