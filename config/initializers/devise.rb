eb0df2612b4fe5afc9c4691c182f920efae11e7d
Koki Ohira <kouking527@icloud.com>
2021-03-18 11:48:52 +0900

    Add valid imformation for facebook sign in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -270,7 +270,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], scope: 'email,public_profile', info_fields: 'email,name', callback_url: "#{ENV['HOST']}/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], token_params: { parse: :json }, scope: 'email,public_profile', info_fields: 'email,name', callback_url: "#{ENV['HOST']}/users/auth/facebook/callback"
   config.omniauth :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"], scope: 'email', oauth_callback: "#{ENV['HOST']}/users/auth/twitter/callback"
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET_KEY"], scope: "userinfo.email, userinfo.profile", redirect_uri: "#{ENV['HOST']}/users/auth/google_oauth2/callback"
   config.omniauth :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_KEY"], scope: 'user', redirect_uri: "#{ENV['HOST']}/users/auth/google_oauth2/callback"
