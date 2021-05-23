7b406ea4aa0ca20517c5ae49bf52c05a658e0b59
Koki Ohira <kouking527@icloud.com>
2021-05-23 12:36:00 +0900

    Add system_spec to dislpay works user liked #53
    To improve application reliability

--- spec/system/users/users_profile_spec.rb
+++ spec/system/users/users_profile_spec.rb
@@ -8,13 +8,17 @@ RSpec.describe 'Users profile', type: :system do
       sign_in user
     end
 
-    it "profile imformation display correctly" do
+    it "profile information display correctly" do
       visit user_path user
       within(".user--info") do
-        expect(page).to have_selector "img"
+        expect(page).to have_selector "img[alt='#{user.username}']"
         expect(page).to have_selector "h1", text: "#{user.username}"
       end
       within(".user--works") do
+        within("ul.user--works__types") do
+          expect(page).to have_link "作品一覧", href: user_path(user)
+          expect(page).to have_link "いいね一覧", href: user_likes_path(user)
+        end
         user.works.page(1) do |work|
           expect(page).to have_selector "img.avatar"
           expect(page).to have_selector ".user", text: "#{work.user.name}"
