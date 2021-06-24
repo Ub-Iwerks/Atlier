9bb9aac54f1f9368fb67ec8e8f9862d367d87e4f
Koki Ohira <kouking527@icloud.com>
2021-06-24 20:18:23 +0900

    Connect service by nginx

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'image_processing'
 gem 'mini_magick'
 gem 'ancestry'
 
+gem 'unicorn'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '>= 4.0.2'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.hosts.clear
+
   config.after_initialize do
     Bullet.enable        = true
     Bullet.alert         = true
