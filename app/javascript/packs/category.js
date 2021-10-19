340da89771b423c25035e889f804cb622993490a
Koki Ohira <kouking527@icloud.com>
2021-10-19 09:41:37 +0900

    fix: functionのリファクタリング

--- app/javascript/packs/category.js
+++ app/javascript/packs/category.js
@@ -10,8 +10,8 @@ $(document).on('turbolinks:load', function () {
         options[1].remove();
       }
     }
-    $("#parent_category_id").on('change',function(){
-      var parentId = document.getElementById('parent_category_id').value;
+    function changeOptoins(parent_category_id, child_category_id){
+      var parentId = document.getElementById(parent_category_id).value;
       if (parentId != ""){
         $.ajax({
           url: '/works/get_category_children/',
@@ -20,8 +20,8 @@ $(document).on('turbolinks:load', function () {
           dataType: 'json'
         })
         .done(function(children){
-          removeOptions('work_create_category_id');
-          var first_option = document.getElementById('work_create_category_id').children[0];
+          removeOptions(child_category_id);
+          var first_option = document.getElementById(child_category_id).children[0];
           var insertHTML = '';
           children.forEach(function(child){
             insertHTML += getOption(child);
@@ -32,50 +32,15 @@ $(document).on('turbolinks:load', function () {
           alert('カテゴリー取得に失敗しました');
         })
       }else{
-        removeOptions('work_create_category_id');
+        removeOptions(child_category_id);
       }
+    }
+
+    $("#parent_category_id").on('change', function(){
+      changeOptoins('parent_category_id', 'work_create_category_id')
+    });
+    $("#work_search_parent_category_id").on('change', function(){
+      changeOptoins('work_search_parent_category_id', 'work_search_category_id')
     });
   });
-/*
-  $(function(){
-    function appendOption(category){
-      var html = `<option value="${category.id}">${category.name}</option>`;
-      return html;
-    }
-    function appendChildrenBox(insertHTML){
-      var childSelectHtml = "";
-      childSelectHtml = `<span class="category__child category__select" id="children_wrapper">
-                          <select name="work_search[category_id]" id="child__category" class="select_field">
-                            <option value="">指定なし</option>
-                            ${insertHTML}
-                          </select>
-                        </span>`;
-      $('.field--category').append(childSelectHtml);
-    }
-    $("#work_search_parent_category_id").on('change',function(){
-      var parentId = document.getElementById('work_search_parent_category_id').value;
-      if (parentId != ""){
-        $.ajax({
-          url: '/works/get_category_children/',
-          type: 'GET',
-          data: { parent_id: parentId },
-          dataType: 'json'
-        })
-        .done(function(children){
-          $('#children_wrapper').remove();
-          var insertHTML = '';
-          children.forEach(function(child){
-            insertHTML += appendOption(child);
-          });
-          appendChildrenBox(insertHTML);
-        })
-        .fail(function(){
-          alert('カテゴリー取得に失敗しました');
-        })
-      }else{
-        $('#children_wrapper').remove();
-      }
-    });
-  });
-*/
 });
