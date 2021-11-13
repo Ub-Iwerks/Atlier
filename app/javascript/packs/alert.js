fc608dfeaccc78edd4b6aa52a7a4b08488022655
Koki Ohira <kouking527@icloud.com>
2021-11-13 09:17:10 +0900

    fix: alert.jsのリファクタリング

--- app/javascript/packs/alert.js
+++ app/javascript/packs/alert.js
@@ -1,11 +1,13 @@
 $(function(){
-  // TODO: 上手く引数が反映されていない（target, thisなど）
   function alertSize(fileField) {
     alert("最大サイズは5MBです。それ以下を選択して下さい。");
     fileField.value = ''
   }
-  $(".image__field").on("change", function() {
-    var size_in_megabytes = this.files[0].size/1024/1024;
-    if (size_in_megabytes > 5) alertSize(this);
+  var targetImageFields = document.querySelectorAll(".image__field");
+  targetImageFields.forEach(selector => {
+    selector.addEventListener('change', function() {
+      var size_in_megabytes = this.files[0].size/1024/1024;
+      if (size_in_megabytes > 5) alertSize(this);
+    });
   });
 });
