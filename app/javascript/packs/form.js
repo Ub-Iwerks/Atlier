bbb0da53bed39831312e617ce94d77ad33f811eb
Koki Ohira <kouking527@icloud.com>
2021-11-22 09:00:32 +0900

    fix: preview.jsの適用・コンソールのエラー解除

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -1,6 +1,6 @@
 $(function(){
   // コンセプトcheckboxがチェックされたら、フォームをグレーアウトさせる
-  var conceptCheckbox = document.querySelector('.checkbox__work_concept')
+  var conceptCheckbox = document.getElementsByClassName('checkbox__work_concept')[0]
   var conceptForm = document.querySelector('#work_create_concept')
   conceptCheckbox.addEventListener('change', function() {
     if (this.checked) conceptForm.disabled = true;
