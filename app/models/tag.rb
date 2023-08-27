9641e218c02da0aa609b8e80af288fb6517e38e4
ohira koki <kouking527@icloud.com>
2023-08-27 18:25:22 +0900

    add: work tag relations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,11 @@
 class Tag < ApplicationRecord
-    validates :title,
-        presence: true,
-        uniqueness: true,
-        length: { maximum: 255 }
+  # Relations
+  has_many :taggings, dependent: :destroy
+  has_many :works, through: :taggings
+
+  # Validation
+  validates :title,
+      presence: true,
+      uniqueness: true,
+      length: { maximum: 255 }
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,9 +1,14 @@
 class Tagging < ApplicationRecord
-    validates :work_id,
-        presence: true,
-        uniqueness: { scope: :tag_id,
-            message: "同様のタグは付与できません"
-        }
-    validates :tag_id,
-        presence: true
+  # Relations
+  belongs_to :tag
+  belongs_to :work
+
+  # Validation
+  validates :work_id,
+      presence: true,
+      uniqueness: { scope: :tag_id,
+          message: "同様のタグは付与できません"
+      }
+  validates :tag_id,
+      presence: true
 end
